a9723ae1d08a4089d6d233f08cb7bcc5e6b5527f
iancmy <ianalbert.c@gmail.com>
2023-02-27 18:55:07 +0800

    Add dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import WebFont from "webfontloader";
+
+WebFont.load({
+  google: {
+    families: ["Lexend", "sans-serif"],
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
